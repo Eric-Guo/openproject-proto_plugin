26e85eea9a854e41852a9409561e0ca31c4fe860
Eric-Guo <eric.guocz@gmail.com>
2024-04-15 23:14:07 +0800

    No hover and console word.

--- frontend/module/global_scripts.ts
+++ frontend/module/global_scripts.ts
@@ -7,20 +7,7 @@ import * as jQuery from 'jquery';
 
 (function($) {
   $(document).ready(function() {
-
-    // OpenProject logo gets thick red border on mouse hover.
-    $(".op-logo").hover(
-      function enter() {
-        $(this).attr("style", "border: 2px solid red;");
-      },
-      function leave() {
-        $(this).removeAttr("style");
-      }
-    );
-
     // Widget box emphasized by giving it a nice red border.
     $('#proto-plugin-block').parent().addClass('proto-plugin-widget-box');
-
-    console.log(I18n.t('js.th_plugin_name') + ' OK');
   });
 })(jQuery);
