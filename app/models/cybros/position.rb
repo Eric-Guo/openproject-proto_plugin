830199eeea7e21210baad7230fad1d2bf122e267
Eric Guo <eric.guocz@gmail.com>
2024-04-30 14:18:37 +0800

    Can get the active_department from cybros

--- app/models/cybros/position.rb
+++ app/models/cybros/position.rb
@@ -4,6 +4,13 @@ module Cybros
   class Position < ApplicationRecord
     belongs_to :department, optional: true
 
+    has_many :position_users, dependent: :destroy
+    has_many :users, through: :position_users
+
     default_scope { joins(:department).where.not(department: { dept_code: "" }).where.not(department: { dept_code: nil }) }
+
+    def self.active_department_list
+      Position.joins(:users).where.not(users: {locked_at: nil}).collect(&:department).uniq
+    end
   end
 end
