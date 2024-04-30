f7751f7caad7005d697c1bd06005606a4bf332ad
Eric Guo <eric.guocz@gmail.com>
2024-04-30 15:23:25 +0800

    Some logic to determine the group_name

--- app/workers/th_plugin/add_active_department_job.rb
+++ app/workers/th_plugin/add_active_department_job.rb
@@ -4,7 +4,15 @@ class ThPlugin::AddActiveDepartmentJob < ApplicationJob
     departments.each do |department|
       dept_name = Bi::PkCodeName.mapping2deptcode.fetch(department.dept_code, department.dept_code)
       company_name = Bi::OrgShortName.company_short_names_by_orgcode.fetch(department.company_code, department.company_code)
-      puts "#{company_name}-#{dept_name}"
+      next if dept_name.include?('撤销')
+
+      group_name = if dept_name.start_with?('天华集团')
+        dept_name
+      else
+        "#{company_name}-#{dept_name}"
+      end
+
+      puts group_name
     end
   end
 end
