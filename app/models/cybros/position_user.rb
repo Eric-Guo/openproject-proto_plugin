b4dc06ab02c77b7e193a3b96cc9fb04960ad3991
Eric Guo <eric.guocz@gmail.com>
2024-05-06 13:13:11 +0800

    Need access position first.

--- app/models/cybros/position_user.rb
+++ app/models/cybros/position_user.rb
@@ -8,7 +8,7 @@ module Cybros
     delegate :name, to: :position, prefix: :position
 
     def self.active_position_user_by_group_name
-      PositionUser.joins(:user).where.not(user: { locked_at: nil }).group_by { |position_user| position_user.department.op_group_name }
+      PositionUser.joins(:user).where.not(user: { locked_at: nil }).group_by { |position_user| position_user.position.department.op_group_name }
     end
   end
 end
