da91f0ea845a592edd195149458a4a88fa075a72
Eric Guo <eric.guocz@gmail.com>
2024-05-06 13:24:05 +0800

    How to get user in op.

--- app/models/cybros/user.rb
+++ app/models/cybros/user.rb
@@ -9,6 +9,7 @@ module Cybros
     has_many :departments, through: :department_users
     has_many :position_users, -> { order(main_position: :desc) }
     has_many :positions, through: :position_users
+    belongs_to :op_user, class_name: 'User', foreign_key: 'mail', primary_key: 'email'
 
     has_one :stfreinstate, -> { active }, class_name: 'Hrdw::HrdwStfreinstateBi', foreign_key: :clerkcode, primary_key: :clerk_code
 
--- app/workers/th_plugin/add_active_user_to_group_job.rb
+++ app/workers/th_plugin/add_active_user_to_group_job.rb
@@ -1,11 +1,12 @@
 class ThPlugin::AddActiveUserToGroupJob < ApplicationJob
   def perform
     user_positions = Cybros::PositionUser.active_position_user_by_group_name
-    user_positions.each do |user_position|
-      group_name = user_position[0]
+    user_positions.each do |group_name, position_users|
       next if group_name.blank?
 
       group = Group.find_by(name: group_name)
+      users_in_op = position_users.collect { |pu| pu.user.op_user.present? }.uniq
+      puts users_in_op
     end
   end
 end
