680bc3f724f61d2d9fc1ceb0315f86b503092266
Eric Guo <eric.guocz@gmail.com>
2024-05-06 11:24:40 +0800

    Extract the group_name to Department#op_group_name

--- app/models/cybros/department.rb
+++ app/models/cybros/department.rb
@@ -6,5 +6,19 @@ module Cybros
     has_many :users, through: :department_users
 
     default_scope { where.not(dept_code: "").where.not(dept_code: nil) }
+
+    def op_group_name
+      return if dept_code.blank?
+
+      dept_name = Bi::PkCodeName.mapping2deptcode.fetch(dept_code, dept_code)
+      company_name = Bi::OrgShortName.company_short_names_by_orgcode.fetch(company_code, company_code)
+      return if dept_name.include?('撤销')
+
+      if dept_name.start_with?('天华集团')
+        dept_name
+      else
+        "#{company_name}-#{dept_name}"
+      end
+    end
   end
 end
--- app/workers/th_plugin/add_active_department_job.rb
+++ app/workers/th_plugin/add_active_department_job.rb
@@ -2,15 +2,8 @@ class ThPlugin::AddActiveDepartmentJob < ApplicationJob
   def perform
     departments = Cybros::Position.active_department_list
     departments.each do |department|
-      dept_name = Bi::PkCodeName.mapping2deptcode.fetch(department.dept_code, department.dept_code)
-      company_name = Bi::OrgShortName.company_short_names_by_orgcode.fetch(department.company_code, department.company_code)
-      next if dept_name.include?('撤销')
-
-      group_name = if dept_name.start_with?('天华集团')
-        dept_name
-      else
-        "#{company_name}-#{dept_name}"
-      end
+      group_name = department.op_group_name
+      next if group_name.blank?
 
       group = Group.find_or_initialize_by(name: group_name)
       group.save
