52df6dab4d49ca3a8c79980035f9b192f1a5e0e1
Eric Guo <eric.guocz@gmail.com>
2024-05-06 11:55:04 +0800

    By group name

--- app/workers/th_plugin/add_active_user_to_group_job.rb
+++ app/workers/th_plugin/add_active_user_to_group_job.rb
@@ -1,4 +1,11 @@
 class ThPlugin::AddActiveUserToGroupJob < ApplicationJob
   def perform
+    user_positions = Cybros::PositionUser.active_position_user_by_group_name
+    user_positions.each do |user_position|
+      group_name = user_position[0]
+      next if group_name.blank?
+
+      group = Group.find_by(name: group_name)
+    end
   end
 end
