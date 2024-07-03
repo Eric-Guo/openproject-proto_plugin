f0e07e63c2e19503ea0fa320343e078f57f9ae64
Eric Guo <eric.guocz@gmail.com>
2024-07-03 15:33:44 +0800

    Full name space.

--- lib/open_project/th_plugin/patches/api/notification_representer.rb
+++ lib/open_project/th_plugin/patches/api/notification_representer.rb
@@ -8,7 +8,7 @@ module OpenProject::ThPlugin
           ->(*) do
             property :reason_name,
                      exec_context: :decorator,
-                     getter: ->(*) { I18n.t("js.notifications.reasons.#{PropertyFactory.reason_for(represented)}") }
+                     getter: ->(*) { I18n.t("js.notifications.reasons.#{::API::V3::Notifications::PropertyFactory::reason_for(represented)}") }
           end
         end
       end
