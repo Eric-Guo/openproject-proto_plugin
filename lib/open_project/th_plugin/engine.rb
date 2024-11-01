b66ee1acbcf8b542dcd8831b12f73697aed43926
Eric Guo <eric.guocz@gmail.com>
2024-11-01 11:57:00 +0800

    Bump to OP 15.1.0

--- lib/open_project/th_plugin/engine.rb
+++ lib/open_project/th_plugin/engine.rb
@@ -14,7 +14,7 @@ module OpenProject::ThPlugin
     register(
       'openproject-th_plugin',
       author_url: 'https://www.thape.com',
-      requires_openproject: '>= 15.0.0',
+      requires_openproject: '>= 15.1.0',
       name: :project_module_th_plugin,
     ) do
       # We define a new project module here for our controller including a permission.
