38cba73f11678faa12bd05085080e8e47147c13d
Eric Guo <eric.guocz@gmail.com>
2025-01-08 18:06:25 +0800

    Revert "Only lock it."
    This reverts commit 88e1ed20bdd2bdf8a17d2c8303cf72621561559c.

--- app/workers/th_plugin/sync_cybros_user_status_job.rb
+++ app/workers/th_plugin/sync_cybros_user_status_job.rb
@@ -3,7 +3,12 @@ class ThPlugin::SyncCybrosUserStatusJob < ApplicationJob
     User.where('mail LIKE ?', '%@thape.com.cn').find_each do |user|
       next if user.staff.blank?
 
-      if user.staff.locked_at.present?
+      if user.staff.locked_at.blank?
+        if user.status != 'active'
+          user.activate
+          user.save
+        end
+      else
         user.locked!
       end
     end
