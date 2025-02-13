ec8c2b364dea6c68747793b2eee7245c04458679
Eric Guo <eric.guocz@gmail.com>
2025-02-13 11:30:22 +0800

    Another bug

--- app/workers/th_plugin/add_active_user_to_group_job.rb
+++ app/workers/th_plugin/add_active_user_to_group_job.rb
@@ -9,7 +9,8 @@ class ThPlugin::AddActiveUserToGroupJob < ApplicationJob
 
       users_in_op_ids = position_users.collect { |pu| pu.user.op_user }.reject(&:nil?).uniq.collect(&:id)
       current_group_cybros_user_ids = group.group_users.collect { |gu| gu.user.staff }.uniq.collect(&:id)
-      remove_user_ids = Cybros::User.where(id: current_group_cybros_user_ids.uniq).where.not(locked_at: nil).pluck(:id)
+      remove_cybros_users = Cybros::User.where(id: current_group_cybros_user_ids.uniq).where.not(locked_at: nil)
+      remove_user_ids = remove_cybros_users.collect { |cu| cu.op_user }.collect(&:id)
       group.add_and_remove_members_to_group(User.system, users_in_op_ids, remove_user_ids)
     end
   end
