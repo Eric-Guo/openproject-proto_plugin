79def1948ad63fb61a2da1595c8f135adb3fad2f
Eric-Guo <eric.guocz@gmail.com>
2026-01-04 11:36:47 +0800

    Adjust non_working_days

--- app/models/bi/th_calendar.rb
+++ app/models/bi/th_calendar.rb
@@ -5,12 +5,16 @@ module Bi
     self.table_name = 'TH_CALENDAR'
 
     def self.non_working_days(check_days)
-      Bi::ThCalendar
-        .order(datestamp: :asc)
-        .where(iswork: 'N').where(datestamp: check_days)
-        .pluck(:datestamp).each_with_object({}) do |s, h|
+      return @non_working_days if @non_working_days.present?
+
+      @non_working_days = Bi::ThCalendar.order(datestamp: :asc)
+                                        .where(iswork: 'N').where(datestamp: check_days)
+                                        .pluck(:datestamp).each_with_object({}) do |s, h|
         h[s] = true
       end
+      @non_working_days[Date.new(2026, 1, 2)] = true if check_days.include?(Date.new(2026, 1, 2))
+      @non_working_days[Date.new(2026, 1, 4)] = false if check_days.include?(Date.new(2026, 1, 4))
+      @non_working_days
     end
   end
 end
