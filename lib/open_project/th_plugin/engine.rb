3b6395bc391144ae0dddd19b5c8c2fefed4fcd17
Eric-Guo <eric.guocz@gmail.com>
2026-07-24 11:51:50 +0800

    Bump to OP 17.7.0

--- lib/open_project/th_plugin/engine.rb
+++ lib/open_project/th_plugin/engine.rb
@@ -14,7 +14,7 @@ module OpenProject::ThPlugin
     register(
       'openproject-th_plugin',
       author_url: 'https://www.thape.com',
-      requires_openproject: '>= 17.6.1',
+      requires_openproject: '>= 17.7.0',
       name: :project_module_th_plugin,
     ) do
       # We define a new project module here for our controller including a permission.
